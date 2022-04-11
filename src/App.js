e684c666858d9cab66a26443a28e7adce2560a25
koapic1 <koapic1@gmail.com>
2022-04-11 10:21:03 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css"; //ES6 문법
 
+//modules 부품화
+//jsx
+//component 여러개의 코드 한번에 몰아 쓰기
+//1. jsx 는 최상위에 부모 하나만 존재해야함. 만약 쓸모없는 태그를 생성하기 싫으면 <></> (프레그먼트) 로 감싸면 됨.
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const myName = "one";
+    return (
+        <>
+            <div className="App">
+                <header className="App-header">
+                    {/* {} 데이터바인딩 */}
+                    <h1>
+                        <span style={{ fontSize: "150px", color: "#61dafb", marginTop: "100px" }}>{myName}</span>
+                    </h1>
+                    <p>{100 + 1}</p>
+                </header>
+            </div>
+        </>
+    );
 }
 
 export default App;
